304d51af6f975384415b6c4dfb8780165df5788d
Cody Kelly <cody.kelly@ucdenver.edu>
2018-02-27 12:15:50 -0700

    Implemented search functions in dictionary class

--- HW5/dictionary.cpp
+++ HW5/dictionary.cpp
@@ -3,19 +3,29 @@
 int dictionary::searchForward(list<DictEntry> &wordList, wordType &findString)
 {
     int count = 0;
+    for (auto it = wordList.begin(); it != wordList.end(); it++, count++)
+    {
+        if (it->getWord() == findString)
+        {
+            return count;   // return number of elements searched
+        }
+    }
     
-    
-    
-    return count;
+    return -1;  // If the given string was not found, return -1
 }
 
 int dictionary::searchBackward(list<DictEntry> &wordList, wordType &findString)
 {
     int count = 0;
+    for (auto it = wordList.rbegin(); it != wordList.rend(); it++, count++)
+    {
+        if (it->getWord() == findString)
+        {
+            return count;   // return number of elements searched
+        }
+    }
     
-    
-    
-    return count;
+    return -1;  // If the given string was not found, return -1
 }
 
 void dictionary::revPrintList(ostream &output, list<DictEntry> &wordList)
